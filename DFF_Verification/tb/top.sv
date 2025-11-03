d176999ff20e3f7f55521260b5230e07eac3a133
Muttu Naik <muttunaik5096@gmail.com>
2025-11-03 21:55:34 +0530

    Add DFF testbench module with clock generation

--- DFF_Verification/tb/top.sv
+++ DFF_Verification/tb/top.sv
@@ -1,1 +1,25 @@
+module dff_top;
+
+        import uvm_pkg::*;
+        import dff_pkg::*;
+
+        `include "uvm_macros.svh"
+
+        bit clk;
+        always #5 clk = ~clk;
+
+        dff_if INF(clk);
+
+        dff DUV(.clk(clk), .rst(INF.rst), .d(INF.d), .q(INF.q));
+
+        initial
+                begin
+                        `ifdef VCS
+                        $fsdbDumpvars(0,dff_top);
+                        `endif
+
+                        uvm_config_db#(virtual dff_if)::set(null,"*","INF",INF);
+                        run_test();
+        end
+endmodule
 
