66d6c33a9a6fe791c41d6da8bcf358f84a585781
Muttu Naik <muttunaik5096@gmail.com>
2025-11-03 22:00:24 +0530

    Update dff_env.sv

--- DFF_Verification/tb/dff_env.sv
+++ DFF_Verification/tb/dff_env.sv
@@ -1,1 +1,22 @@
+class dff_env extends uvm_env;
+        `uvm_component_utils(dff_env)
+
+        dff_agent agth;
+        dff_scoreboard  sbh;
+
+function new(string name = "dff_env", uvm_component parent = null);
+        super.new(name, parent);
+endfunction
+
+function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        agth = dff_agent :: type_id :: create("agth", this);
+        sbh = dff_scoreboard :: type_id :: create("sbh",this);
+endfunction
+
+function void connect_phase (uvm_phase phase);
+        agth.monh.ap.connect(sbh.ai);
+endfunction
+
+endclass
 
