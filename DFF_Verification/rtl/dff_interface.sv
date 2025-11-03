d946f0743e0b16a930f01c49ba6d53e124f781a1
Muttu Naik <muttunaik5096@gmail.com>
2025-11-03 21:50:37 +0530

    DFF interface

--- DFF_Verification/rtl/dff_interface.sv
+++ DFF_Verification/rtl/dff_interface.sv
@@ -1,1 +1,21 @@
+interface dff_if(input bit clk);
+        logic rst;
+        logic d;
+        logic q;
+
+        clocking drv_cb@(posedge clk);
+                output rst;
+                output d;
+        endclocking
+
+        modport DRV_MP(clocking drv_cb);
+
+        clocking mon_cb@(posedge clk);
+                input rst;
+                input d;
+                input q;
+        endclocking
+
+        modport MON_MP(clocking mon_cb);
+endinterface
 
