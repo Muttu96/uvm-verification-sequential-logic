279f5983b95a526592b598650aca20edd840cc37
Muttu Naik <muttunaik5096@gmail.com>
2025-11-03 22:40:22 +0530

    Add counter_pkg package with UVM components

---
+++ Counter_Verification/test/counter_pkg.sv
@@ -0,0 +1,21 @@
+package counter_pkg;
+
+        import uvm_pkg::*;
+        `include "uvm_macros.svh"
+
+        `include "agent_config.sv"
+
+        `include "counter_xtn.sv"
+
+        `include "sequence.sv"
+        `include "sequencer.sv"
+        `include "monitor.sv"
+        `include "driver.sv"
+        `include "agent.sv"
+
+        `include "scoreboard.sv"
+
+        `include "env.sv"
+        `include "test.sv"
+
+endpackage
