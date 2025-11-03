33cf2f2915c59a91c6179386cf17523aaa3362a3
Muttu Naik <muttunaik5096@gmail.com>
2025-11-03 22:37:40 +0530

    Add top module for counter verification testbench

--- Counter_Verification/tb/counter_top.sv
+++ Counter_Verification/tb/counter_top.sv
@@ -1,1 +1,24 @@
+module top;
+
+        import uvm_pkg::*;
+        import counter_pkg::*;
+
+        `include "uvm_macros.svh"
+
+        bit clk;
+        always #5 clk=~clk;
+
+        counter_if INF(clk);
+
+        counter DUV(.clk(clk), .rstn(INF.rstn), .load(INF.load), .up_down(INF.up_down), .data_in(INF.data_in), .data_out(INF.data_out));
+
+        initial
+                begin
+                        `ifdef VCS
+                        $fsdbDumpvars(0,top);
+                        `endif
+                        uvm_config_db#(virtual counter_if)::set(null,"*","counter_if",INF);
+                        run_test();
+                end
+endmodule
 
